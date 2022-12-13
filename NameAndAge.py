6d41f240cf7e92e114cbd8bbf72759bd1e1a8ef7
AdamMcWhirter <113976924+AdamMcWhirter@users.noreply.github.com>
2022-12-13 09:48:23 +0000

    Add files via upload

---
+++ NameAndAge.py
@@ -0,0 +1,16 @@
+Forename = "Adam"
+Surname = "McWhirter"
+
+Age = 16
+
+Fullname = Forename + " " + Surname
+
+print ("My first name is " + Forename)
+print ("My surname is " + Surname)
+print ("This year I am " + str(Age) + " Years old")
+
+Age += 1
+
+print("Next year I will be " + str(Age) + " years old")
+
+print("End of application")
